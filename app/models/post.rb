0141cc41149bebc0ae75f04257c8489436fa2632
Semyz Serhii <80937588895@mail.ru>
2018-04-19 01:45:15 +0300

    views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+  attr_accessible :title, :body, :author_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/Users/a/RubymineProjects/blog/app/views/commits/index.html.erb'
+  resources :commits
+  root 'commits#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
