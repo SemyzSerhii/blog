216e8d3fb2139a634f7dfdd1071fe082693bb1df
Semyz Serhii <80937588895@mail.ru>
2018-05-13 18:23:56 +0300

    remove useless model

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.test_framework  false
+      g.view_specs      false
+      g.helper_specs    false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
