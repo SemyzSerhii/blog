420d4039bb2e0ce41926976d7d2f93ef3cae57f5
Semyz Serhii <semizserhii@gmail.com>
2018-07-09 20:16:28 +0300

    user_signed_in? method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :user_signed_in?
   protect_from_forgery with: :exception
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def user_signed_in?
+    current_user.present?
   end
 end
 
